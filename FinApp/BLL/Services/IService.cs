118e847683fdb85ce6ec8cdae51eced8cf1fadc5
NazarOleksiv <nazzaroleksiv@gmail.com>
2019-07-25 14:24:15 +0300

    Added some services and repositories

---
+++ FinApp/BLL/Services/IService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    interface IService<TResult> where TResult : class
+    {
+        Task<IEnumerable<TResult>> ReadAsync();
+        Task<TResult> ReadAsync(int id);
+        Task<TResult> CreateAsync(TResult user);
+        Task<TResult> UpdateAsync(TResult user);
+        Task<TResult> DeleteAsync(int id);
+    }
+}
