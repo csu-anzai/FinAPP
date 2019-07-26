256592b0d2ef29e3f3d87f5992f01ec4b2e464a0
Roman4oo <rom.ivanets@gmail.com>
2019-07-26 07:48:22 +0300

    Added Hashing and UserService

--- FinApp/BLL/Services/ImplementedServices/UserService.cs
+++ FinApp/BLL/Services/ImplementedServices/UserService.cs
@@ -6,13 +6,28 @@ using DAL.Repositories.IRepositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BLL.Security;
+using System.Threading.Tasks;
 
 namespace BLL.Services.ImplementedServices
 {
     public class UserService: Service<User>, IUserService
     {
-        public UserService(IUnitOfWork unitOfWork, IUserRepository userRepository): base(unitOfWork, userRepository)
+        protected IPassHasher _hasher;
+        IUnitOfWork _unitOfWork;
+
+
+        public UserService(IUnitOfWork unitOfWork, IUserRepository userRepository, IPassHasher hasher): base(unitOfWork, userRepository)
         {
+            _hasher = hasher;
+            _unitOfWork = unitOfWork;
+            
         }
+       public async Task<User> SignInAsync(User user)
+        {
+            user.Password = _hasher.HashPassword(user.Password);
+            return await base.CreateAsync(user);
+        }
+        
     }
 }
