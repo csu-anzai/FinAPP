9b08cc018ff3f33925312182e718ec29eb4c33e1
Evan <sudorchykivan@gmail.com>
2019-07-28 21:54:30 +0300

    Fixed BaseRepository and AuthRepository

--- FinApp/Data/Repositories/BaseRepository.cs
+++ FinApp/Data/Repositories/BaseRepository.cs
@@ -12,52 +12,52 @@ namespace DAL.Repositories
     abstract public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
     {
         private readonly DbContext _context;
-        protected readonly DbSet<TEntity> entities;
+        private readonly DbSet<TEntity> _entities;
 
         public BaseRepository(DbContext context)
         {
             _context = context;
-            entities = context.Set<TEntity>();
+            _entities = context.Set<TEntity>();
         }
 
         public virtual async Task AddAsync(TEntity entity)
         {
-            await entities.AddAsync(entity);
+            await _entities.AddAsync(entity);
         }
 
         public virtual async Task AddRangeAsync(IEnumerable<TEntity> entities)
         {
-            await this.entities.AddRangeAsync(entities);
+            await _entities.AddRangeAsync(entities);
         }
 
         public virtual async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> expression)
         {
-            return await entities.Where(expression).ToListAsync();
+            return await _entities.Where(expression).ToListAsync();
         }
 
         public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
         {
-            return await entities.ToListAsync();
+            return await _entities.ToListAsync();
         }
 
         public virtual async Task<TEntity> GetAsync(int id)
         {
-            return await entities.FindAsync(id);
+            return await _entities.FindAsync(id);
         }
 
         public virtual void Remove(TEntity entity)
         {
-            entities.Remove(entity);
+            _entities.Remove(entity);
         }
 
         public void RemoveRange(IEnumerable<TEntity> range)
         {
-            entities.RemoveRange(range);
+            _entities.RemoveRange(range);
         }
 
         public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> expression)
         {
-            return await entities.SingleOrDefaultAsync(expression);
+            return await _entities.SingleOrDefaultAsync(expression);
         }
     }
 }
--- FinApp/Data/Repositories/ImplementedRepositories/AuthRepository.cs
+++ FinApp/Data/Repositories/ImplementedRepositories/AuthRepository.cs
@@ -1,12 +1,13 @@
-﻿using DAL.Entities;
+﻿using DAL.Context;
+using DAL.Entities;
 using DAL.Repositories.IRepositories;
-using Microsoft.EntityFrameworkCore;
+
 
 namespace DAL.Repositories.ImplementedRepositories
 {
     public class AuthRepository : BaseRepository<User>, IAuthRepository
     {
-        public AuthRepository(DbContext context) : base(context)
+        public AuthRepository(FinAppContext context) : base(context)
         {
         }
     }
