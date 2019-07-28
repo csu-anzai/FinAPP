01e5c85836d7cc6028f3b552d8732b3cb23e7464
iKorniak <kornelia1204@gmail.com>
2019-07-28 23:25:07 +0300

    Added signUp page

--- FinApp/FinApp/ClientApp/src/app/app.module.ts
+++ FinApp/FinApp/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -11,6 +11,8 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
 import { AuthService } from './_services/auth.service';
 
 @NgModule({
@@ -21,17 +23,20 @@ import { AuthService } from './_services/auth.service';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: WelcomePageComponent, pathMatch: 'full' },
       { path: 'login-page', component: LoginPageComponent },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'sign-up', component: SignUpComponent },
     ])
   ],
   providers: [
