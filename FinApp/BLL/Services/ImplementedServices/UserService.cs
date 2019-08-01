4080ddcdb46b3639162949d165ab6d19cb48ea00
Alexander Antonyuk <aasandroqq@gmail.com>
2019-08-01 11:42:18 +0300

    Added validation email in UserService

--- FinApp/BLL/Services/ImplementedServices/UserService.cs
+++ FinApp/BLL/Services/ImplementedServices/UserService.cs
@@ -5,12 +5,13 @@ using DAL.Repositories.IRepositories;
 using DAL.UnitOfWork;
 using System.Threading.Tasks;
 
+
 namespace BLL.Services.ImplementedServices
 {
     public class UserService : Service<User>, IUserService
     {
         protected IPassHasher _hasher;
-
+        //private readonly ILogger logger;
         public UserService(IUnitOfWork unitOfWork, IUserRepository userRepository, IPassHasher hasher) : base(unitOfWork, userRepository)
         {
             _hasher = hasher;
@@ -18,6 +19,10 @@ namespace BLL.Services.ImplementedServices
 
         public async Task<User> CreateUserAsync(User user)
         {
+            var existedUser = await _repository.SingleOrDefaultAsync(u => u.Email == user.Email);
+            if (existedUser != null)
+                return null;
+
             user.Password = _hasher.HashPassword(user.Password);
             return await CreateAsync(user);
         }
