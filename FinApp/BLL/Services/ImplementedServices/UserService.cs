5432d1e633b227717b4edd800c3192d1775759c8
Alexander Antonyuk <aasandroqq@gmail.com>
2019-08-01 11:56:24 +0300

    Validation and write error in logger

--- FinApp/BLL/Services/ImplementedServices/UserService.cs
+++ FinApp/BLL/Services/ImplementedServices/UserService.cs
@@ -3,6 +3,7 @@ using BLL.Services.IServices;
 using DAL.Entities;
 using DAL.Repositories.IRepositories;
 using DAL.UnitOfWork;
+using Serilog;
 using System.Threading.Tasks;
 
 
@@ -11,7 +12,7 @@ namespace BLL.Services.ImplementedServices
     public class UserService : Service<User>, IUserService
     {
         protected IPassHasher _hasher;
-        //private readonly ILogger logger;
+        private readonly ILogger logger;
         public UserService(IUnitOfWork unitOfWork, IUserRepository userRepository, IPassHasher hasher) : base(unitOfWork, userRepository)
         {
             _hasher = hasher;
@@ -21,8 +22,10 @@ namespace BLL.Services.ImplementedServices
         {
             var existedUser = await _repository.SingleOrDefaultAsync(u => u.Email == user.Email);
             if (existedUser != null)
+            {
+                logger.Fatal("Email already existed, do not create User");
                 return null;
-
+            }
             user.Password = _hasher.HashPassword(user.Password);
             return await CreateAsync(user);
         }
