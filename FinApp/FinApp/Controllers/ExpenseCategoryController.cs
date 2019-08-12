bebfaaf5013ae54f2deed1b7df1095c84b3d5bc0
Roman <dvtytrhenbqrjvg@gmail.com>
2019-08-12 12:19:21 +0300

    Add Income Category Controller and Expense Category Controller

--- FinApp/FinApp/Controllers/ExpenseCategoryController.cs
+++ FinApp/FinApp/Controllers/ExpenseCategoryController.cs
@@ -24,6 +24,54 @@ namespace FinApp.Controllers
             _expenseCategoryService = expenseCategoryService;
         }
 
-       
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExpenseCategory(int id)
+        {
+            var expenseCategoryInDb = await _expenseCategoryService.GetExpenseCategoryAsync(id);
+            if (expenseCategoryInDb == null)
+                return NotFound();
+            await _expenseCategoryService.DeleteExpenseCategoryAsync(expenseCategoryInDb);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateExpenseCategory(int id, ExpenseCategoryDTO expenseCategoryDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var expenseCategory = await _expenseCategoryService.UpdateExpenseCategoryAsync(expenseCategoryDTO);
+            if (expenseCategory == null)
+                return BadRequest(new { message = "Category Id is incorrect!" });
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllExpenseCategory()
+        {
+            var expenseCategories = await _expenseCategoryService.GetAllExpenseCategoryAsync();
+            if (expenseCategories == null)
+                return NoContent();
+            return Ok(expenseCategories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetExpenseCategory(int id)
+        {
+            var expenseCategory = await _expenseCategoryService.GetExpenseCategoryAsync(id);
+            if (expenseCategory == null)
+                return NotFound();
+            var expenseCategoryDTO = _mapper.Map<ExpenseCategory, ExpenseCategoryDTO>(expenseCategory);
+            return Ok(expenseCategoryDTO);
+        }
+
+        [HttpPost("createExpenseCategory")]
+        public async Task<IActionResult> CreateExpenseCategory(ExpenseCategoryDTO expenseCategoryDTO)
+        {
+            var expenseCategory = _mapper.Map<ExpenseCategory>(expenseCategoryDTO);
+            var newExpenseCategory = await _expenseCategoryService.CreateExpenseCategoryAsync(expenseCategory);
+            if (newExpenseCategory == null)
+                return BadRequest(new { message = "Category is already exist!" });
+            return Ok();
+        }
     }
 }
--- FinApp/FinApp/Controllers/IncomeCategoryController.cs
+++ FinApp/FinApp/Controllers/IncomeCategoryController.cs
@@ -23,5 +23,55 @@ namespace FinApp.Controllers
             _mapper = mapper;
             _incomeCategoryService = incomeCategoryService;
         }
-    }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteIncomeCategory(int id)
+        {
+            var incomeCategoryInDb = await _incomeCategoryService.GetIncomeCategoryAsync(id);
+            if (incomeCategoryInDb == null)
+                return NotFound();
+            await _incomeCategoryService.DeleteIncomeCategoryAsync(incomeCategoryInDb);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateIncomeCategory(int id, IncomeCategoryDTO incomeCategoryDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var incomeCategory = await _incomeCategoryService.UpdateIncomeCategoryAsync(incomeCategoryDTO);
+            if (incomeCategory == null)
+                return BadRequest(new { message = "Category Id is incorrect!" });
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllIncomeCategory()
+        {
+            var incomeCategories = await _incomeCategoryService.GetAllIncomeCategoryAsync();
+            if (incomeCategories == null)
+                return NoContent();
+            return Ok(incomeCategories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetIncomeCategory(int id)
+        {
+            var incomeCategory = await _incomeCategoryService.GetIncomeCategoryAsync(id);
+            if (incomeCategory == null)
+                return NotFound();
+            var incomeCategoryDTO = _mapper.Map<IncomeCategory, IncomeCategoryDTO>(incomeCategory);
+            return Ok(incomeCategoryDTO);
+        }
+
+        [HttpPost("createIncomeCategory")]
+        public async Task<IActionResult> CreateIncomeCategory(IncomeCategoryDTO incomeCategoryDTO)
+        {
+            var incomeCategory = _mapper.Map<IncomeCategory>(incomeCategoryDTO);
+            var newIncomeCategory = await _incomeCategoryService.CreateIncomeCategoryAsync(incomeCategory);
+            if (newIncomeCategory == null)
+                return BadRequest( new { message = "Category is already exist!"});
+            return Ok();
+        }
+    } 
 }
