2804c83eddd0a3ba43679b2f6a3c82d7bfcff33e
IraKorniak <kornelia1204@gmail.com>
2019-08-12 11:24:33 +0300

    Added account controller, service, mapper, DTOs

---
+++ FinApp/Data/Repositories/IRepositories/IAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Entities;
+using DAL.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.IRepositories
+{
+    public interface IAccountRepository: IBaseRepository<Account>
+    {
+        Task<Account> GetAccountById(int id);
+        Task<Account> FindAsyncAccountWithImgCurrency(Expression<Func<Account, bool>> expression);
+    }
+}
