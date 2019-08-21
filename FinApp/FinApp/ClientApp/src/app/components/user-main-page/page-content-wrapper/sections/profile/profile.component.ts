5acdea8cc27ed77c6df942a38c8c8dd928d89456
Evan <sudorchykivan@gmail.com>
2019-08-21 13:15:50 +0300

    Fixed Date format in profile

--- FinApp/FinApp/ClientApp/src/app/components/user-main-page/page-content-wrapper/sections/profile/profile.component.ts
+++ FinApp/FinApp/ClientApp/src/app/components/user-main-page/page-content-wrapper/sections/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { User } from 'src/app/models/user';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 import { NotificationService } from 'src/app/services/notification.service';
+import  * as moment from 'moment';
 
 @Component({
   selector: 'app-profile',
@@ -14,7 +15,7 @@ export class ProfileComponent implements OnInit {
 
   profileForm: FormGroup;
   user: User = new User();
-
+  date: any;
   constructor(
     private _authService: AuthService,
     private _userService: UserService,
@@ -23,7 +24,7 @@ export class ProfileComponent implements OnInit {
 
     this.profileForm = fb.group({
       'Name': new FormControl('', Validators.compose([Validators.required, Validators.pattern('[a-zA-z-]*')])),
-      'BirthDate': new FormControl('', Validators.required),
+      'BirthDate': new FormControl('', Validators.compose([Validators.required])),
       'Surname': new FormControl('', Validators.compose([Validators.required, Validators.pattern('[a-zA-z-]*')])),
       'Email': new FormControl('', Validators.compose([Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]))
     });
@@ -36,19 +37,20 @@ export class ProfileComponent implements OnInit {
 
       this.profileForm.get('Name').setValue(this.user.name);
       this.profileForm.get('Surname').setValue(this.user.surname);
-      this.profileForm.get('BirthDate').setValue(`${this.user.birthDate}`);
+    //  this.profileForm.get('BirthDate').setValue(this.user.birthDate);
+      this.date = this.user.birthDate.toString().split('-').reverse().join('-');
+      this.profileForm.get('BirthDate').setValue(this.date);
       this.profileForm.get('Email').setValue(this.user.email);
     });
   }
-  
 
   updateProfile() {
     if (this.profileForm.valid) {
       this.user.name = this.profileForm.controls['Name'].value,
         this.user.surname = this.profileForm.controls['Surname'].value,
-        this.user.birthDate = this.profileForm.controls['BirthDate'].value,
         this.user.email = this.profileForm.controls['Email'].value,
-
+       // this.user.birthDate = new Date(moment(this.profileForm.controls['BirthDate'].value,'DD/MM/YYYY').format('MM/DD/YYYY'));
+        this.user.birthDate = new Date((this.profileForm.controls['BirthDate'].value).split('-').reverse().join('-'));
         this._userService.update(this.user).subscribe(() => {
           console.log(this.user);
           this.alertService.successMsg('Profile updated');
@@ -77,4 +79,7 @@ export class ProfileComponent implements OnInit {
 //  });
 //   }
 
-
+// this.user.birthDate
+    //   this.date = new Date(moment(this.profileForm.controls['BirthDate'].value,'DD/MM/YYYY').format('MM/DD/YYYY'));
+    //   console.log(this.date);
+      //   this.date = new Date(moment(this.profileForm.controls['BirthDate'].value).format("MM/dd/YYYY")),
