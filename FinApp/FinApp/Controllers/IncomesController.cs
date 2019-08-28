0ec820ad82a8d07ea8f222fddf60488b8b5d107a
IraKorniak <kornelia1204@gmail.com>
2019-08-28 14:29:52 +0300

    Added add-income component, income service and fixed styles

---
+++ FinApp/FinApp/Controllers/IncomesController.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Models.ViewModels;
+using BLL.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FinApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncomesController : Controller
+    {
+        [HttpPost("add")]
+        public async Task<IActionResult> AddIncome()
+        {
+            return Ok(new { message = "Adding income was successful" });
+        }
+
+    }
+}
