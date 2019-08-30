04c2d32cd4365eb0d885f6d9b083cc0815157e67
Nazar Oleksiv <naoleksiv@gmail.com>
2019-08-30 08:24:12 +0300

    Added a property filter

---
+++ FinApp/Data/Repositories/Filters/IncludeMultiples.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Filters
+{
+    public static class IncludeMultiples
+    {
+        public static IQueryable<TEntity> IncludeMultiple<TEntity>
+            (this IQueryable<TEntity> query, params Expression<Func<TEntity, object>>[] includes) where TEntity : class
+        {
+            if (includes != null)
+            {
+                query = includes.Aggregate(query, (current, include) => current.Include(include));
+            }
+            return query;
+        }
+
+        public static IQueryable<TEntity> WhereIncludeMultiple<TEntity>
+            (this IQueryable<TEntity> query, Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes) where TEntity : class
+        {
+            query = query.Where(predicate);
+            return IncludeMultiple(query, includes);
+        }
+
+        public static async Task<TEntity> IncludeMultiple<TEntity>
+            (this IQueryable<TEntity> query, Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes) where TEntity : class
+        {
+            return await WhereIncludeMultiple(query, predicate, includes).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ FinApp/Data/Repositories/Filters/UserFilter.cs
@@ -0,0 +1,47 @@
+﻿using DAL.Context;
+using DAL.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace DAL.Repositories.Filters
+{
+    public class UserFilter
+    {
+        FinAppContext _context;
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+        public string HashedPassword { get; set; }
+        public string UserEmail { get; set; }
+        public DateTime UserBirthDate { get; set; }
+        public string UserAvatar { get; set; }
+        public int UserRoleId { get; set; }
+
+        public Expression PropertiesFilter()
+        {
+            var chainOfConditions = PredicateBuilder.False<User>();
+
+            if (!string.IsNullOrEmpty(UserName))
+                chainOfConditions = chainOfConditions.And(i => i.Name == UserName);
+
+            if (!string.IsNullOrEmpty(UserSurname))
+                chainOfConditions = chainOfConditions.And(i => i.Surname == UserSurname);
+
+            if (!string.IsNullOrEmpty(UserEmail))
+                chainOfConditions = chainOfConditions.And(i => i.Email == UserEmail);
+
+            if (!string.IsNullOrEmpty(UserAvatar))
+                chainOfConditions = chainOfConditions.And(i => i.Avatar == UserAvatar);
+
+            if (!string.IsNullOrEmpty(HashedPassword))
+                chainOfConditions = chainOfConditions.And(i => i.Password == HashedPassword);
+
+            if (UserRoleId != default)
+                chainOfConditions = chainOfConditions.And(i => i.RoleId == UserRoleId);
+
+            if (UserBirthDate != default)
+                chainOfConditions = chainOfConditions.And(i => i.BirthDate == UserBirthDate);
+
+            return chainOfConditions;
+        }
+    }
+}
