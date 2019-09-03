f3444ef7812c504742b88e5a3c644e76b7f8e0ba
Nazar Oleksiv <naoleksiv@gmail.com>
2019-09-03 20:01:44 +0300

    Set up dynamic translation in notifications

--- FinApp/FinApp/ClientApp/src/app/components/user-main-page/page-content-wrapper/sections/contact-us/contact-us.component.ts
+++ FinApp/FinApp/ClientApp/src/app/components/user-main-page/page-content-wrapper/sections/contact-us/contact-us.component.ts
@@ -3,9 +3,9 @@ import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms'
 import { NotificationService } from 'src/app/services/notification.service';
 import { UserService } from 'src/app/services/user.service';
 import { AuthService } from 'src/app/services/auth.service';
-import { ChangePassword } from 'src/app/models/changePassword';
 import { EmailConfirmationService } from 'src/app/services/email-confirmation.service';
 import { Email } from 'src/app/models/email';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-us',
@@ -15,23 +15,18 @@ import { Email } from 'src/app/models/email';
 export class ContactUsComponent implements OnInit {
   emailInfo: Email;
   contactForm: FormGroup;
+  successfullySent: string;
+  failedSent: string;
 
   constructor(
     private _authService: AuthService,
     private _userService: UserService,
     private emailSender: EmailConfirmationService,
     private formBuilder: FormBuilder,
+    private translate: TranslateService,
     private _alertService: NotificationService) {
-
-    this.contactForm = formBuilder.group({
-      'FullName': new FormControl('', Validators.compose(
-        [Validators.required, Validators.pattern("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")])),
-
-      'Email': new FormControl('', Validators.compose(
-        [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])),
-
-      'Message': new FormControl('', Validators.required)
-    });
+    this.setUpContactForm();
+    this.translateSubscription();
   }
 
   ngOnInit() {
@@ -49,10 +44,29 @@ export class ContactUsComponent implements OnInit {
       this.emailInfo.message = this.contactForm.controls['Message'].value;
 
       this.emailSender.sendMailToAdmin(this.emailInfo).subscribe(
-        next => {},
-        err => this._alertService.errorMsg('Your message wasn\'t sent'),
-        () => this._alertService.infoMsg('Your message was sent')
+        next => { },
+        err => this._alertService.errorMsg(this.failedSent),
+        () => this._alertService.infoMsg(this.successfullySent)
       );
     }
+  }
+
+  setUpContactForm() {
+    this.contactForm = this.formBuilder.group({
+      'FullName': new FormControl('', Validators.compose(
+        [Validators.required, Validators.pattern("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")])),
+
+      'Email': new FormControl('', Validators.compose(
+        [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])),
+
+      'Message': new FormControl('', Validators.required)
+    });
+  }
+
+  translateSubscription() {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.translate.get('contactUs.successfullyMsg').subscribe((text: string) => this.successfullySent = text);
+      this.translate.get('contactUs.errorMsg').subscribe((text: string) => this.failedSent = text);
+    });
   }
 }
